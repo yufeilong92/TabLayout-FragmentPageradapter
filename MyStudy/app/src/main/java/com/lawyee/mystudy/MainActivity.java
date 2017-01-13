099992c5c136c9a6be5e5cf515bad411da1d9ec7
931697478@qq.com <931697478@qq.com>
2017-01-13 16:05:08 +0800

    页面滑动tablayout 也刷滑动

--- MyStudy/app/src/main/java/com/lawyee/mystudy/MainActivity.java
+++ MyStudy/app/src/main/java/com/lawyee/mystudy/MainActivity.java
@@ -1,124 +1,127 @@
-package com.lawyee.mystudy;
+package com.lawyee.myapplication;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.tablayout)
-    TabLayout tablayout;
+    @BindView(R.id.titles)
+    TabLayout titles;
 
-    @BindView(R.id.viewpager)
-    ViewPager viewpager;
-    @BindView(R.id.activity_main)
-    LinearLayout activityMain;
-    private TabLayout mTablayout;
-    private ViewPager mViewpager;
-    private ArrayList<View> views;
+    @BindView(R.id.vpPRODUNCTdatails)
+    ViewPager vpprodiunctidata;
+    private String[] mTitles = {"商品", "详情", "评论"};
+    private TextView tv;
+    private TextView tv2;
+    private TextView tv3;
+public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initView();
-        setStatusBarColor();
+        initview();
+
 
     }
 
-    private void setStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccent));
-        }
+    private void initview() {
+
+        initViewPager();
+        initToobar();
     }
 
-    private void initView() {
-        mTablayout = (TabLayout) findViewById(R.id.tablayout);
+    private void initToobar() {
+        titles.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
+        titles.addTab(titles.newTab().setCustomView(R.layout.layout1));
+        tv = (TextView) findViewById(R.id.textview);
+        tv.setText("商品");
+        titles.addTab(titles.newTab().setCustomView(R.layout.layout2));
+        tv2 = (TextView) findViewById(R.id.textview2);
+        tv2.setText("详情");
+        titles.addTab(titles.newTab().setCustomView(R.layout.layout3));
+        tv3 = (TextView) findViewById(R.id.textview3);
+        tv3.setText("评论");
 
-        mTablayout.setOnTabSelectedListener(listener);
-        mViewpager = (ViewPager) findViewById(R.id.viewpager);
+        titles.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.e(TAG, "onTabSelected: " );
+                int position = tab.getPosition();
+                switch (position) {
+                    case 0:
+                        Toast.makeText(MainActivity.this, "你选中的" + tv.getText(), Toast.LENGTH_SHORT).show();
+                        tv.setTextColor(getResources().getColor(R.color.colorAccent));
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "你选中的" + tv2.getText(), Toast.LENGTH_SHORT).show();
+                        tv2.setTextColor(getResources().getColor(R.color.colorAccent));
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "你选中的" + tv3.getText(), Toast.LENGTH_SHORT).show();
+                        tv3.setTextColor(getResources().getColor(R.color.colorAccent));
+                        break;
+                    default:
+                        Log.e("====", "onTabSelected: "+position);
+                        tv.setTextColor(getResources().getColor(R.color.colorAccent));
+                        vpprodiunctidata.setCurrentItem(position);
+                        break;
+                }
+            }
 
-        addtab();
-        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
-        View view = inflater.inflate(R.layout.layout1, null);
-        View view1 = inflater.inflate(R.layout.layout2, null);
-        View view2 = inflater.inflate(R.layout.layout3, null);
-        views = new ArrayList<>();
-        views.add(view);
-        views.add(view1);
-        views.add(view2);
-        MypagerAdapter adapter = new MypagerAdapter();
-        mTablayout.setupWithViewPager(mViewpager);
-        mViewpager.setAdapter(adapter);
-        mTablayout.getTabAt(1).select();
+
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tv2.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tv3.setTextColor(getResources().getColor(R.color.colorPrimary));
+                Log.e(TAG, "onTabUnselected: " );
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Log.e(TAG, "onTabReselected: " );
+
+            }
+        });
+        //设置选中
+     titles.getTabAt(2).select();
+    }
+
+    private void initViewPager() {
+        vpprodiunctidata.setAdapter(new PDFragmentPagerAdapter(getSupportFragmentManager(), mTitles));
+        vpprodiunctidata.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+//            在页面滚动
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                 titles.setScrollPosition(position,positionOffset,true);
+            }
+
+            @Override
+//            选中页面
+            public void onPageSelected(int position) {
+                Log.e("=====", "onPageSelected: " );
+                titles.getTabAt(position).select();
+            }
+
+            @Override
+//            页面滚动改变中
+            public void onPageScrollStateChanged(int state) {
+             titles.setScrollX(state);
+            }
+        });
 
     }
 
-    private void addtab() {
-        int i = 3;
-        for (int i1 = 0; i1 < i; i1++) {
-            mTablayout.addTab(mTablayout.newTab().setText("你好 " + i1));
-        }
-    }
-
-    private TabLayout.OnTabSelectedListener listener =new TabLayout.OnTabSelectedListener() {
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-     Toast.makeText(MainActivity .this,"onTabSelected"+tab.getText(),Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-     Toast.makeText(MainActivity .this,"onTabUnselected"+tab.getText(),Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-     Toast.makeText(MainActivity .this,"onTabReselected"+tab.getText(),Toast.LENGTH_SHORT).show();
-
-    }
-};
-
-
-
-    class MypagerAdapter extends PagerAdapter {
-        @Override
-        public int getCount() {
-            return views.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-
-            container.addView(views.get(position));
-            return views.get(position);
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(views.get(position));
-        }
-    }
-
-    ;
 
 }
